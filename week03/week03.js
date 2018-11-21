ecf47cd45e0b7dc8eb992f6f2dc7693eb76b7b3a
swe-jaeyoungpark <swe.jaeyoungpark@navercorp.com>
2018-11-21 20:45:02 +0900

    Add TaskList

--- week03/week03.js
+++ week03/week03.js
@@ -1,3 +1,41 @@
+const TaskList = class {
+    constructor(title) {
+        if (!title) {
+            error('invalid title');
+        }
+
+        this._title = title;
+        this._list = [];
+    }
+
+    add(title, date) {
+        this._list.push(Task.get(title, date));
+    }
+
+    remove(task) {
+        const list = this._list;
+        if (list.includes(task)) {
+            list.splice(list.indexOf(task), 1);
+        }
+    }
+
+    byTitle(stateGroup = true) {
+        return this._getList(Sort.title, stateGroup);
+    }
+
+    byDate(stateGroup = true) {
+        return this._getList(Sort.date, stateGroup);
+    }
+
+    _getList(sort, stateGroup) {
+        const list = this._list;
+        return !stateGroup ? [...list].sort(sort) : [
+            ...list.filter(v => !v.isComplete()).sort(sort),
+            ...list.filter(v => v.isComplete()).sort(sort),
+        ];
+    }
+};
+
 const Sort = class {
     static title = (a, b) => a.sortTitle(b);
     static date = (a, b) => a.sortDate(b);
